791355f77b336a7a51afd474b13040410c73cfc8
eloilsondosanjos <eloilson.ageedu@gmail.com>
2022-02-17 09:15:55 -0300

    initial commit

---
+++ jsninjaes6/main.js
@@ -0,0 +1,4 @@
+import { soma } from './matematica';
+
+const res = soma(12, 2);
+console.log(res);
---
+++ jsninjaes6/webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  target: 'node',
+  externals: [nodeExternals()],
+  entry: "./main.js",
+  output: {
+    path: __dirname,
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+}
